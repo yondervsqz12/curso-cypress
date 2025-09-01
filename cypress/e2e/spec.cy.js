a02f3f73ba0cc6835ddaf36e0c3f2ff37c512a45
Yonder Vasquez <Yonder.v16@gmail.com>
2025-08-31 19:43:33 -0600

    Fix report and flakyness

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,16 @@
+import { LoginMethods } from "./pages/login/login.methods";
+
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+
+    const username = "random01";
+    const password = "random01";
+
+    cy.visit('https://demoblaze.com/')
+    cy.get('a[data-target="#logInModal"]').click();
+    LoginMethods.login(username, password);
+    cy.get('a#nameofuser').should('contain.text', username);
+    cy.wait(3000);
+
   })
 })
