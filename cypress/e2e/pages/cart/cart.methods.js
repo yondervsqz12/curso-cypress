825784b9cfbbd09f428b65e9fe32d0ca8585a0e0
Yonder Vasquez <Yonder.v16@gmail.com>
2025-09-01 23:30:48 -0600

    Creacion de pagina Cart

---
+++ cypress/e2e/pages/cart/cart.methods.js
@@ -0,0 +1,11 @@
+import { CartElements } from "./cart.elements";
+
+export class CartMethods {
+
+static clickOnDeleteLink(productName) {
+    CartElements.links.delete(productName).click();
+}
+
+
+
+}
