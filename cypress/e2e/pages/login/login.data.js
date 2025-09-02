dbeed086e2b760843bc65037ab7e021596d00d51
Yonder Vasquez <Yonder.v16@gmail.com>
2025-09-01 21:19:43 -0600

    Creacion de pagina sign up

--- cypress/e2e/pages/login/login.data.js
+++ cypress/e2e/pages/login/login.data.js
@@ -6,5 +6,4 @@ export class LoginData {
             password: "abc"
         };
     }
-
 }
---
+++ cypress/e2e/pages/sign-up/signup.methods.js
@@ -0,0 +1,25 @@
+import { SignupElements } from "./signup.elemets";
+
+export class SignupMethods {
+
+    static insertUsername(username) {
+        SignupElements.textBoxes.username.invoke('val', username)
+    }
+
+    static insertPassword(password) {
+        SignupElements.textBoxes.password.invoke('val', password)
+    }
+
+    static clickSignupButton() {
+        SignupElements.buttons.signup.click()
+    }
+
+    static signup(username, password) {
+
+        this.insertUsername(username)
+        this.insertPassword(password)
+        this.clickSignupButton()
+
+    }
+
+}
