9215f01609e025633c98fde0d7fa27d7449d4d0a
Yonder Vasquez <Yonder.v16@gmail.com>
2025-09-01 22:00:45 -0600

    Creacion de archivos del HomePage

---
+++ cypress/e2e/pages/home-page/home.elements.js
@@ -0,0 +1,21 @@
+export class HomeElements {
+
+    static get categoriesMenu() {
+        return {
+            get phones() {
+                return cy.contains('a', 'Phones')
+            },
+            get laptops() {
+                return cy.contains('a', 'Laptops')
+            },
+            get monitors() {
+                return cy.contains('a', 'Monitors')
+            }
+        }
+    }
+
+    static product(productName) {
+        return cy.contains('a', productName)
+    }
+
+}
