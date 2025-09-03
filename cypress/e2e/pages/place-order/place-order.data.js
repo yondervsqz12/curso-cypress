83f43d967db348db2ac3be353a20bb42d0ff9ee3
Yonder Vasquez <Yonder.v16@gmail.com>
2025-09-02 20:27:11 -0600

    Creacion de la pagina Place Order

---
+++ cypress/e2e/pages/place-order/place-order.data.js
@@ -0,0 +1,15 @@
+export class PlaceOrderData{
+
+    static get testData() {
+        return {
+            name: "Test Name",
+            country: "Costa Rica",
+            city: "Alajuela",
+            creditCardNumber: "4012888888881881",
+            month: 12,
+            year: 2024,
+        }
+    }
+
+
+}
