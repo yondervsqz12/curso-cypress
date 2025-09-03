7f252b17767d4f9dcad46ba9f3f9a790cbb848e4
Yonder Vasquez <Yonder.v16@gmail.com>
2025-09-02 20:40:57 -0600

    Caso de prueba "Registro de Usuario Valido"

---
+++ cypress/e2e/tests/registro-y-autenticacion.cy.js
@@ -0,0 +1,29 @@
+import { CommonPageData } from "../pages/common-page/common-page.data"
+import { CommonPageMethods } from "../pages/common-page/common-page.methods"
+import { SignupMethods } from "../pages/signup/signup.methods"
+import { Logger } from "../utilities/logger"
+
+describe(CommonPageData.testSuites.registroYAutenticacion, () =>{
+    it('Registro de usuario válido', ()=> {
+
+        Logger.stepNumber(1)
+        Logger.step("Navegar a la página de inicio")
+        CommonPageMethods.navigateToDemoBlaze()
+
+        Logger.stepNumber(2)
+        Logger.step("Hacer clic en Sign Up en la barra de navegacion")
+        CommonPageMethods.clickOnSignupOption()
+
+        Logger.stepNumber(3)
+        Logger.step("Completar todos los campos obligatorios con informacion válida")
+        SignupMethods.insertUsername('Admin123456')
+        SignupMethods.insertPassword('Admin123456')
+
+        Logger.stepNumber(4)
+        Logger.step("Hacer clic en el boton Sign up")
+        SignupMethods.clickSignupButton()
+        Logger.verification('Verificar que se muestre el mensaje "Sign up successful"')
+        SignupMethods.verifySignupSuccessfulMessageIsDisplayed()
+        
+    })
+})
