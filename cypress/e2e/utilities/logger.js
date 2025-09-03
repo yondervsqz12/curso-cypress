c764224b303b12a547d12dcb9160848ee6622197
Yonder Vasquez <Yonder.v16@gmail.com>
2025-09-02 20:34:58 -0600

    Agregar Logger

---
+++ cypress/e2e/utilities/logger.js
@@ -0,0 +1,33 @@
+export class Logger{
+
+    static stepNumber(number) {
+        const text = `Step # ${number}`
+        cy.log(text)
+        cy.allure().step(text)
+    }
+
+    static step(description) {
+        const text = `Step - ${description}`
+        cy.log(text)
+        cy.allure().step(text)
+    }
+
+    static verification(description) {
+        const text = `Verification - ${description}`
+        cy.log(text)
+        cy.allure().step(text)
+    }
+
+    static subStep(description) {
+        const text = `subStep - ${description}`
+        cy.log(text)
+        cy.allure().step(text)
+    }
+
+    static subVerification(description) {
+        const text = `subVerification - ${description}`
+        cy.log(text)
+        cy.allure().step(text)
+    }
+
+}
