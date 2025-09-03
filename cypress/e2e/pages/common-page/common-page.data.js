5180fb37de358c4209e82495ba7ad42a955f808d
Yonder Vasquez <Yonder.v16@gmail.com>
2025-09-02 20:31:02 -0600

    Creacion de la pagina Common Page

---
+++ cypress/e2e/pages/common-page/common-page.data.js
@@ -0,0 +1,15 @@
+export class CommonPageData {
+
+    static get url() {
+        return 'https://demoblaze.com/index.html'
+    }
+
+    static get testSuites() {
+        return {
+            registroYAutenticacion: "Registro y Autenticación",
+            catalogoYCompra: "Catalogo y Compra"
+
+        }
+    }
+
+}
---
+++ cypress/e2e/pages/common-page/common-page.methods.js
@@ -0,0 +1,41 @@
+import { CommonPageData } from "./common-page.data";
+import { CommonPageElements } from "./common-page.elements";
+
+export class CommonPageMethods {
+    
+    static navigateToDemoBlaze() {
+        cy.clearCookies();
+        cy.visit(CommonPageData.url)
+    }
+
+    static clickOnHomeOption() {
+        CommonPageElements.topMenu.home.click()
+    }
+
+    static clickOnContactOption() {
+        CommonPageElements.topMenu.contact.click()
+    }
+
+    static clickOnAboutUsOption() {
+        CommonPageElements.topMenu.aboutUs.click()
+    }
+
+    static clickOnCartOption() {
+        CommonPageElements.topMenu.cart.click()
+    }
+
+    static clickOnLoginOption() {
+        CommonPageElements.topMenu.login.click()
+    }
+
+    static clickOnSignupOption() {
+        CommonPageElements.topMenu.signup.click() 
+    }
+
+    static verifyAlert(expectedMessage) {
+        cy.on('window:alert', (str) => {
+            expect(str).to.equal(expectedMessage)
+        })
+    }
+
+}
